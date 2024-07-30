0dd7ab02aacc562154433d8bbd6152d2c93e3592
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-07-30 19:02:33 +0100

    update PR

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
-// import AuthContextProvider from "@/context/AuthContext";
+import AuthContextProvider from "@/context/AuthContext";
 
 const inter = Roboto({
   subsets: ["latin"],
@@ -15,7 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <AuthContextProvider>
+        <body className={inter.className}>{children}</body></AuthContextProvider>
     </html>
   );
 }
