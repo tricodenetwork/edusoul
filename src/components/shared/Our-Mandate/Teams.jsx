85fee8f092cb41280ea7090a87da4922b5b42c8e
AKDebug-UX <akoredesalaudeen54@gmail.com>
2024-08-01 11:04:59 +0100

    update PR

--- src/components/shared/Our-Mandate/Teams.jsx
+++ src/components/shared/Our-Mandate/Teams.jsx
@@ -15,12 +15,12 @@ export default function Teams({ mobile }) {
             src="/assets/images/ideas/ideas-1.png"
           />
 
-          <div className="flex p-4 bg-rose-200 lg:h-[535px] flex-col justify-center items-start gap-8">
+          <div className="flex p-4 bg-rose-200 h-[535px] flex-col justify-center items-start gap-8">
             <div className="flex-col justify-start items-start gap-4 flex">
               <h4 className="text-primary text-xl font-bold uppercase tracking-[3px]">
                 Team work
               </h4>
-              <h3 className="text-primary text-[36px] md:text-[56px] leading-none xl:w-[80%] font-extrabold">
+              <h3 className="text-primary text-[36px] lg:text-[56px] leading-none xl:w-[80%] font-extrabold">
                 Easily transition with your team{" "}
               </h3>
             </div>
